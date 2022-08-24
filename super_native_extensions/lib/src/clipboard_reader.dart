75541d216d859d824b9893cb1cc268a14381c728
Matej Knopp <matej.knopp@gmail.com>
2022-08-24 14:56:49 +0200

    Reformat

--- super_native_extensions/lib/src/clipboard_reader.dart
+++ super_native_extensions/lib/src/clipboard_reader.dart
@@ -8,6 +8,7 @@ abstract class ClipboardReader {
   /// Returns clipboard reader for current clipboard. Note that on some platforms
   /// the clipboard content for single reader will not change during the lifetime
   /// of the reader. On top of it the content is cached lazily.
-  ///  If you need updated information create a new reader.
+  ///
+  /// If you need updated information create a new reader.
   Future<DataReader> newClipboardReader();
 }
