775e7c04891ddf7912cb2779f85929258f9b342a
Matej Knopp <matej.knopp@gmail.com>
2022-09-08 14:14:11 +0200

    Update example

--- super_keyboard_layout/example/lib/main.dart
+++ super_keyboard_layout/example/lib/main.dart
@@ -6,10 +6,6 @@ late KeyboardLayoutManager _keyboardLayoutManager;
 
 void main() async {
   _keyboardLayoutManager = await KeyboardLayoutManager.instance();
-  print('Supported: ${_keyboardLayoutManager.supported}');
-  _keyboardLayoutManager.onLayoutChanged.addListener(() {
-    print('Layout changed');
-  });
   runApp(const MyApp());
 }
 
@@ -29,46 +25,122 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LayoutDemoWidget extends StatefulWidget {
+  const LayoutDemoWidget({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _LayoutDemoWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _LayoutDemoWidgetState extends State<LayoutDemoWidget> {
+  late FocusNode _focusNode;
+
   @override
   void initState() {
     super.initState();
+    _focusNode = FocusNode();
+    _focusNode.onKey = _onKey;
+    _focusNode.requestFocus();
     _keyboardLayoutManager.onLayoutChanged.addListener(_layoutChanged);
+  }
+
+  KeyEventResult _onKey(FocusNode node, RawKeyEvent event) {
+    if (event is RawKeyDownEvent) {
+      // print('Key down: ${event.logicalKey}');
+      setState(() {
+        _lastKey = event.physicalKey;
+      });
+    }
+    return KeyEventResult.handled;
   }
 
   @override
   void dispose() {
-    super.dispose();
+    _focusNode.dispose();
     _keyboardLayoutManager.onLayoutChanged.removeListener(_layoutChanged);
+    super.dispose();
   }
+
+  PhysicalKeyboardKey? _lastKey;
 
   void _layoutChanged() {
     setState(() {});
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text("Keyboard layout has changed."),
+        duration: Duration(milliseconds: 1500),
+      ),
+    );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.red, width: 2),
+      ),
+      child: Focus(
+        focusNode: _focusNode,
+        child: _lastKey == null
+            ? const Text('Press any key')
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Pressed physical key:",
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text(_lastKey!.toString()),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text('Logical key for current keyboard layout:',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text(_keyboardLayoutManager.currentLayout
+                          .getLogicalKeyForPhysicalKey(_lastKey!)
+                          ?.toString() ??
+                      'null'),
+                  const Text(
+                      'Logical key for current keyboard layout (with shift):',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text(_keyboardLayoutManager.currentLayout
+                          .getLogicalKeyForPhysicalKey(_lastKey!, shift: true)
+                          ?.toString() ??
+                      'null'),
+                  const Text(
+                      'Logical key for current keyboard layout (with alt):',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text(_keyboardLayoutManager.currentLayout
+                          .getLogicalKeyForPhysicalKey(_lastKey!, alt: true)
+                          ?.toString() ??
+                      'null'),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'Because this functionality is meant for '
+                    'keyboard shortcuts, only ASCII capable keyboard '
+                    'layouts are supported.\nOn Linux switching between '
+                    'keyboards is only recognized after typing a key from'
+                    ' new layout.',
+                    style: TextStyle(fontSize: 11.5),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
@@ -79,8 +151,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 'KeyboardLayoutManager is not supported on this platform.',
               ),
             if (_keyboardLayoutManager.supported)
-              Text(
-                  'Physical key Y on your keyboard will result inֿ\n${_keyboardLayoutManager.currentLayout.getLogicalKeyForPhysicalKey(PhysicalKeyboardKey.bracketLeft)}'),
+              const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: LayoutDemoWidget(),
+              ),
           ],
         ),
       ),
