d988a5fdeedcac0aa776051465364a6068ba7a39
Matej Knopp <matej.knopp@gmail.com>
2022-11-04 11:27:07 +0100

    Remove import

--- super_native_extensions/lib/src/native/context.dart
+++ super_native_extensions/lib/src/native/context.dart
@@ -1,7 +1,6 @@
 import 'dart:ffi';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 import 'package:nativeshell_core/nativeshell_core.dart';
 
 MessageChannelContext _getNativeContext() {
