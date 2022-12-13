f7574dfcef30462af808152a248020a0fe9ec606
Matej Knopp <matej.knopp@gmail.com>
2022-12-13 14:42:25 +0100

    feat: super_drag_and_drop: reexport formats from super_clipboard (#32)

--- super_drag_and_drop/lib/super_drag_and_drop.dart
+++ super_drag_and_drop/lib/super_drag_and_drop.dart
@@ -1,5 +1,7 @@
 library super_drag_and_drop;
 
+export 'package:super_clipboard/src/standard_formats.dart';
+
 export 'src/drag_configuration.dart';
 export 'src/base_draggable_widget.dart';
 export 'src/draggable_widget.dart';
