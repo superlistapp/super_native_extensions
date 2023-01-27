30ab2002daef23b7450398770df100d552756333
Matej Knopp <matej.knopp@gmail.com>
2023-01-27 16:23:37 +0100

    feat: add support for inplace file reading on ios (#55)

--- super_native_extensions/rust/src/darwin/ios/reader.rs
+++ super_native_extensions/rust/src/darwin/ios/reader.rs
@@ -1,14 +1,18 @@
 use std::{
-    fs,
+    cell::RefCell,
+    fs::{self, File},
+    io::Read,
     path::PathBuf,
     rc::{Rc, Weak},
     sync::{Arc, Mutex},
+    thread,
 };
 
+use async_trait::async_trait;
 use block::ConcreteBlock;
 use cocoa::{
-    base::{id, nil},
-    foundation::{NSArray, NSURL},
+    base::{id, nil, BOOL},
+    foundation::{NSArray, NSUInteger, NSURL},
 };
 
 use core_foundation::{base::TCFType, string::CFString};
@@ -17,6 +21,7 @@ use irondash_run_loop::{
     util::{Capsule, FutureCompleter},
     RunLoop,
 };
+
 use objc::{
     class, msg_send,
     rc::{autoreleasepool, StrongPtr},
@@ -35,6 +40,7 @@ use crate::{
     },
     reader_manager::{ReadProgress, VirtualFileReader},
     util::{get_target_path, Movable},
+    value_promise::Promise,
 };
 
 pub struct PlatformDataReader {
@@ -151,7 +157,7 @@ impl PlatformDataReader {
         autoreleasepool(|| unsafe {
             let providers = self.get_items_providers();
             if item < providers.len() as i64 {
-                // travels between threads, must be refcounted because lock is Fn
+                // travels between threads, must be refcounted because block is Fn
                 let completer = Arc::new(Mutex::new(Capsule::new(completer)));
                 let provider = providers[item as usize];
                 let sender = RunLoop::current().new_sender();
@@ -227,8 +233,8 @@ impl PlatformDataReader {
         format: &str,
     ) -> NativeExtensionsResult<bool> {
         // All data on iOS can be received as a virtual file through
-        // loadFileRepresentationForTypeIdentifier so we add a bit of heurists
-        // to avoid creating a temporary file for text, composite content, images
+        // load(InPlace)FileRepresentationForTypeIdentifier so we add a bit of heuristics
+        // to avoid creating a potential temporary file for text, composite content, images
         // and URLS. The assumption here is that they are small enough to be
         // to be all loaded in memory.
         if Self::uti_conforms_to(format, "public.composite-content")
@@ -245,19 +251,58 @@ impl PlatformDataReader {
 
     pub async fn can_read_virtual_file_for_item(
         &self,
-        _item: i64,
-        _format: &str,
+        item: i64,
+        format: &str,
     ) -> NativeExtensionsResult<bool> {
-        Ok(false)
+        self.can_copy_virtual_file_for_item(item, format).await
     }
 
     pub async fn create_virtual_file_reader_for_item(
         &self,
-        _item: i64,
-        _format: &str,
-        _progress: Arc<ReadProgress>,
+        item: i64,
+        format: &str,
+        read_progress: Arc<ReadProgress>,
     ) -> NativeExtensionsResult<Option<Rc<dyn VirtualFileReader>>> {
-        Ok(None)
+        let (future, completer) = FutureCompleter::new();
+        autoreleasepool(|| unsafe {
+            let providers = self.get_items_providers();
+            if item >= providers.len() as i64 {
+                completer.complete(Err(NativeExtensionsError::OtherError(
+                    "Invalid item".into(),
+                )));
+                return;
+            }
+            // travels between threads, must be refcounted because block is Fn
+            let completer = Arc::new(Mutex::new(Capsule::new(completer)));
+            let provider = providers[item as usize];
+            let sender = RunLoop::current().new_sender();
+            let block = ConcreteBlock::new(move |url: id, _is_in_place: BOOL, err: id| {
+                let res = if err != nil {
+                    Err(NativeExtensionsError::VirtualFileReceiveError(
+                        nserror_description(err),
+                    ))
+                } else {
+                    FileWithBackgroundCoordinator::new(url)
+                };
+                let completer = completer.clone();
+                sender.send(move || {
+                    let completer = completer
+                        .lock()
+                        .unwrap()
+                        .take()
+                        .expect("Block invoked more than once");
+                    // completer.complete(res);
+                    let res = res.map::<Option<Rc<dyn VirtualFileReader>>, _>(|f| Some(Rc::new(f)));
+                    completer.complete(res);
+                });
+            });
+            let block = block.copy();
+            let ns_progress: id = msg_send![provider,
+                                    loadInPlaceFileRepresentationForTypeIdentifier:*to_nsstring(format)
+                                    completionHandler:&*block];
+            bridge_progress(ns_progress, read_progress);
+        });
+        future.await
     }
 
     pub async fn copy_virtual_file_for_item(
@@ -270,52 +315,165 @@ impl PlatformDataReader {
         let (future, completer) = FutureCompleter::new();
         autoreleasepool(|| unsafe {
             let providers = self.get_items_providers();
-            if item < providers.len() as i64 {
-                // travels between threads, must be refcounted because lock is Fn
-                let completer = Arc::new(Mutex::new(Capsule::new(completer)));
-                let provider = providers[item as usize];
-                let sender = RunLoop::current().new_sender();
-                let block = ConcreteBlock::new(move |url: id, err: id| {
-                    let res = if err != nil {
-                        Err(NativeExtensionsError::VirtualFileReceiveError(
-                            nserror_description(err),
-                        ))
-                    } else {
-                        let source_path = path_from_url(url);
-                        let source_name = source_path
-                            .file_name()
-                            .expect("Missing file name")
-                            .to_string_lossy();
-                        let target_path = get_target_path(&target_folder, &source_name);
-                        match fs::rename(&source_path, &target_path) {
-                            Ok(_) => Ok(target_path),
-                            Err(err) => Err(NativeExtensionsError::VirtualFileReceiveError(
-                                err.to_string(),
-                            )),
-                        }
-                    };
-                    let completer = completer.clone();
-                    sender.send(move || {
-                        let completer = completer
-                            .lock()
-                            .unwrap()
-                            .take()
-                            .expect("Block invoked more than once");
-                        completer.complete(res);
-                    });
-                });
-                let block = block.copy();
-                let ns_progress: id = msg_send![provider, loadFileRepresentationForTypeIdentifier:*to_nsstring(format)
-                                      completionHandler:&*block];
-                bridge_progress(ns_progress, read_progress);
-            } else {
+            if item >= providers.len() as i64 {
                 completer.complete(Err(NativeExtensionsError::OtherError(
                     "Invalid item".into(),
                 )));
+                return;
             }
+            // travels between threads, must be refcounted because block is Fn
+            let completer = Arc::new(Mutex::new(Capsule::new(completer)));
+            let provider = providers[item as usize];
+            let sender = RunLoop::current().new_sender();
+            let block = ConcreteBlock::new(move |url: id, err: id| {
+                let res = if err != nil {
+                    Err(NativeExtensionsError::VirtualFileReceiveError(
+                        nserror_description(err),
+                    ))
+                } else {
+                    let source_path = path_from_url(url);
+                    let source_name = source_path
+                        .file_name()
+                        .expect("Missing file name")
+                        .to_string_lossy();
+                    let target_path = get_target_path(&target_folder, &source_name);
+                    match fs::rename(&source_path, &target_path) {
+                        Ok(_) => Ok(target_path),
+                        Err(err) => Err(NativeExtensionsError::VirtualFileReceiveError(
+                            err.to_string(),
+                        )),
+                    }
+                };
+                let completer = completer.clone();
+                sender.send(move || {
+                    let completer = completer
+                        .lock()
+                        .unwrap()
+                        .take()
+                        .expect("Block invoked more than once");
+                    completer.complete(res);
+                });
+            });
+            let block = block.copy();
+            let ns_progress: id = msg_send![provider,
+                                    loadFileRepresentationForTypeIdentifier:*to_nsstring(format)
+                                    completionHandler:&*block];
+            bridge_progress(ns_progress, read_progress);
         });
         future.await
     }
 
     pub fn assign_weak_self(&self, _weak: Weak<PlatformDataReader>) {}
+}
+
+struct FileWithBackgroundCoordinator {
+    // used to block the coordinator thread
+    coordinator_thread_release: Arc<Promise<()>>,
+    file: RefCell<Option<File>>,
+    path: PathBuf,
+}
+
+impl FileWithBackgroundCoordinator {
+    /// Creates new thread where it keeps the coordinator alive while the
+    /// FileWithBackgroundCoordinator is reading file.
+    fn new(url: id) -> NativeExtensionsResult<FileWithBackgroundCoordinator> {
+        let promise = Arc::new(Promise::new());
+        let url = unsafe { Movable::new(url) };
+        let promise_clone = promise.clone();
+        let promise_clone2 = promise.clone();
+        thread::spawn(move || {
+            let block = ConcreteBlock::new(move |new_url: id| {
+                let path = path_from_url(new_url);
+                let release = Arc::new(Promise::new());
+                let file = File::open(&path);
+                match file {
+                    Ok(file) => {
+                        promise_clone2.set(Ok(FileWithBackgroundCoordinator {
+                            coordinator_thread_release: release.clone(),
+                            file: RefCell::new(Some(file)),
+                            path,
+                        }));
+                        // wait until the file is closed
+                        release.wait();
+                    }
+                    Err(err) => {
+                        promise_clone2.set(Err(NativeExtensionsError::from(err)));
+                    }
+                }
+            });
+            let block = block.copy();
+            let mut error: id = nil;
+            autoreleasepool(|| unsafe {
+                let coordinator: id = msg_send![class!(NSFileCoordinator), alloc];
+                let coordinator: id = msg_send![coordinator, initWithFilePresenter: nil];
+                let coordinator = StrongPtr::new(coordinator);
+                let () = msg_send![*coordinator,
+                    coordinateReadingItemAtURL:*url
+                    options:1 as NSUInteger /* NSFileCoordinatorReadingWithoutChanges */
+                    error: &mut error
+                    byAccessor: &*block
+                ];
+                if error != nil {
+                    promise_clone.set(Err(NativeExtensionsError::VirtualFileReceiveError(
+                        nserror_description(error),
+                    )));
+                }
+            });
+        });
+        promise.wait()
+    }
+}
+
+impl Drop for FileWithBackgroundCoordinator {
+    fn drop(&mut self) {
+        self.close().ok();
+    }
+}
+
+#[async_trait(?Send)]
+impl VirtualFileReader for FileWithBackgroundCoordinator {
+    async fn read_next(&self) -> NativeExtensionsResult<Vec<u8>> {
+        let mut file = self.file.borrow_mut();
+        match file.as_mut() {
+            Some(file) => {
+                let mut buf = vec![0; 1024 * 1024];
+                file.read(&mut buf)
+                    .map(|size| {
+                        buf.truncate(size);
+                        buf
+                    })
+                    .map_err(NativeExtensionsError::from)
+            }
+            None => {
+                return Err(NativeExtensionsError::VirtualFileReceiveError(
+                    "File already closed".into(),
+                ));
+            }
+        }
+    }
+
+    fn file_size(&self) -> NativeExtensionsResult<Option<i64>> {
+        let file = self.file.borrow();
+        match file.as_ref() {
+            Some(file) => {
+                let metadata = file.metadata()?;
+                Ok(Some(metadata.len() as i64))
+            }
+            None => Ok(None),
+        }
+    }
+
+    fn file_name(&self) -> Option<String> {
+        self.path
+            .file_name()
+            .map(|name| name.to_string_lossy().into_owned())
+    }
+
+    fn close(&self) -> NativeExtensionsResult<()> {
+        let file = self.file.borrow_mut().take();
+        if let Some(_file) = file {
+            self.coordinator_thread_release.set(());
+        }
+        Ok(())
+    }
 }
