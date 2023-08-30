2460a4790c242d4be9db0d88a31738813dad66e2
Matej Knopp <matej.knopp@gmail.com>
2023-08-30 21:17:04 +0200

    update comment

--- check_precompiled.sh
+++ check_precompiled.sh
@@ -5,7 +5,7 @@ BASEDIR=$(dirname "$0")
 cd $BASEDIR/super_native_extensions/cargokit/build_tool
 
 # Check whether the precompiled binaries ara available for each architecture.
-# Note: aaarch64-unknown-linux-gnu... is meant to be missing as there is no
+# Note: aaarch64-unknown-linux-gnu binary is missing as there is no
 # cross-compilation available currently.
 
 dart run build_tool verify-binaries --manifest-dir=../../rust
