50ae3614e0ba1ec7a29b935f01536900f49e521a
Matej Knopp <matej.knopp@gmail.com>
2023-09-12 19:16:07 +0200

    Squashed 'super_native_extensions/cargokit/' changes from e2c0fc9..62bb5c9
    62bb5c9 chore: test iOS flavor and nightly for release build (#20)
    fa301d5 fix: [android] specify compiler extension on windows (#22)
    98f6e01 feat: allow configuring verbose mode through CARGOKIT_VERBOSE env (#21)
    6d421f9 chore: add CI test for building example plugin (#17)
    1f644b2 feat: add workaround for pub path resolving bug (#19)
    c1e8fcf fix: script argument escaping (#18)
    34cc336 Merge pull request #16 from irondash/build_configuraiton_parse_ignore_flavor
    be425f7 fix: parsing configuration should ignore flavor
    32b6720 Merge pull request #15 from irondash/fix_pod_dylib_bundle_replacement
    bfd4bcc fix: [pod] exception when building bundle dylib
    52b2251 docs: comment typo
    
    git-subtree-dir: super_native_extensions/cargokit
    git-subtree-split: 62bb5c9b6c9b3eca1071baf78606bc7f43632133

--- run_build_tool.sh
+++ run_build_tool.sh
@@ -44,7 +44,7 @@ EOF
 
 # Dart run will not cache any package that has a path dependency, which
 # is the case for our build_tool_runner. So instead we precompile the package
-# ourself.
+# ourselves.
 # To invalidate the cached kernel we use the hash of ls -LR of the build_tool
 # package directory. This should be good enough, as the build_tool package
 # itself is not meant to have any path dependencies.
@@ -72,4 +72,4 @@ if [ ! -f "$PACKAGE_HASH_FILE" ]; then
     echo "$PACKAGE_HASH" > "$PACKAGE_HASH_FILE"
 fi
 
-"$DART" bin/build_tool_runner.dill $@
+"$DART" bin/build_tool_runner.dill "$@"
