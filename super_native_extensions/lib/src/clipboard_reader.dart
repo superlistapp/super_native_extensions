1eb120309cf127ebb68dcb90e6c71d0eea97525c
Matej Knopp <matej.knopp@gmail.com>
2023-12-05 23:36:50 +0100

    feat: implement paste event on web (#246)

--- super_native_extensions/lib/src/clipboard_reader.dart
+++ super_native_extensions/lib/src/clipboard_reader.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'native/clipboard_reader.dart'
     if (dart.library.js) 'web/clipboard_reader.dart';
 import 'reader.dart';
@@ -11,4 +13,18 @@ abstract class ClipboardReader {
   ///
   /// If you need updated information create a new reader.
   Future<DataReader> newClipboardReader();
+
+  /// Returns whether paste event is supported on current platform. This is
+  /// only supported on web.
+  bool get supportsPasteEvent;
+
+  /// Registers a listener for paste event. This is only supported on web.
+  /// It is a no-op on other platforms.
+  ///
+  /// The clipboard access will not display any any paste prompt UI,
+  /// unlike accessing clipboard through [newClipboardReader].
+  void registerPasteEventListener(void Function(DataReader) listener);
+
+  /// Removes a listener for paste event. This is currently only supported on web.
+  void unregisterPasteEventListener(void Function(DataReader) listener);
 }
