d3fadbbb469ee1e177c6d7e165ed9fdb7acc91de
Matej Knopp <matej.knopp@gmail.com>
2024-10-21 10:16:30 +0200

    fix: workaround for exception when running in test environment

--- super_native_extensions/lib/src/native/clipboard_events.dart
+++ super_native_extensions/lib/src/native/clipboard_events.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
@@ -47,6 +48,9 @@ class _ClipboardReadEvent extends ClipboardReadEvent {
 
 class ClipboardEventsImpl extends ClipboardEvents {
   ClipboardEventsImpl() {
+    if (Platform.environment.containsKey('FLUTTER_TEST')) {
+      return;
+    }
     _channel.setMethodCallHandler(_onMethodCall);
     _channel.invokeMethod('newClipboardEventsManager');
   }
