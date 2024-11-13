1250d815af643a4f895c12dd26b752de2e3b7074
Matej Knopp <matej.knopp@gmail.com>
2024-11-13 20:00:31 +0100

    fix: widget snapshots not working properly with WASM (#469)

--- super_native_extensions/lib/src/widget_snapshot/widget_snapshotter_web.dart
+++ super_native_extensions/lib/src/widget_snapshot/widget_snapshotter_web.dart
@@ -1,8 +1,10 @@
 import 'dart:js_interop' as js;
 import 'dart:js_interop_unsafe';
 
+import 'package:flutter/foundation.dart';
+
 final isCanvasKit = js.globalContext['flutterCanvasKit'] != null;
 
 bool snapshotToImageSupportedInternal() {
-  return isCanvasKit;
+  return isCanvasKit || kIsWasm;
 }
