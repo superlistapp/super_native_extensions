227d2033c21ec9651314e05acc3856c95a81f83c
chanderlud <me@chanchan.dev>
2025-03-09 15:11:09 -0700

    adds an option to override glibc version to cargokit build tool, and tries to get zigbuild installed for compiling

--- super_native_extensions/cargokit/build_tool/lib/src/rustup.dart
+++ super_native_extensions/cargokit/build_tool/lib/src/rustup.dart
@@ -43,6 +43,18 @@ class Rustup {
     _installedTargets(toolchain)?.add(target);
   }
 
+  bool _didInstallZigBuild = false;
+
+  void installZigBuild() {
+    if (_didInstallZigBuild) {
+      return;
+    }
+
+    log.info("Installing Zig build");
+    runCommand("rustup", ['cargo', 'install', '--locked', 'cargo-zigbuild']);
+    _didInstallZigBuild = true;
+  }
+
   final List<_Toolchain> _installedToolchains;
 
   Rustup() : _installedToolchains = _getInstalledToolchains();
